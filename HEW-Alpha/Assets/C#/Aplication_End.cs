394b3fc88726a70fcbc0b135c9fb2f5d62d5ad5f
Nebosuke-3 <rsangameing@gmail.com>
2022-02-28 14:45:31 +0900

    Build5回目

---
+++ HEW-Alpha/Assets/C#/Aplication_End.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aplication_End : MonoBehaviour
+{
+    public void Click()
+    {
+        Application.Quit();
+    }
+}
